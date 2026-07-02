80c2ab44cbe1cae91c77a6d0ab4377934895cc28
Nerdomancer <kerosamyriad@gmail.com>
2026-07-02 18:19:56 +0300

    messy argument parsing. TODO: refactor

--- paradoxpomo.py
+++ paradoxpomo.py
@@ -2,15 +2,26 @@ from subprocess import Popen
 from time import sleep
 from sys import argv
 
-def notify():
-    Popen("mpv notif.mp3 >> /dev/null", shell=True)
 
-i = 0
 pomo_count = 0
 work_minutes = 25
 rest_minutes = 5
 long_rest_minutes = 30
+pomo_count_per_cycle = 4
+notif_sound = "notif.mp3"
 
+def notify():
+    Popen(f"mpv {notif_sound} >> /dev/null", shell=True)
+
+if len(argv) >= 2 and len(argv) == 5:
+    work_minutes = int(argv[1])
+    rest_minutes = int(argv[2])
+    long_rest_minuts = int(argv[3])
+    pomo_count_per_cycle = int(argv[4])
+elif len(argv) >= 2 and len(argv) != 5:
+    print("Usage: paradoxpomo.py <work minutes> <rest minutes> <long rest minutes> <pomodoro count before long rest>")
+    exit()
+    
 work_message = f"notify-send \"Paradox Pomodoro\" \"It's time to work for {work_minutes} minutes\""
 rest_message = f"notify-send \"Paradox Pomodoro\" \"It's time to rest for {rest_minutes} minutes"
 long_rest_message = f"notify-send \"Paradox Pomodoro\" \"It's time to rest for {long_rest_minutes} minutes"
@@ -21,13 +32,11 @@ while(True):
         Popen(work_message, shell=True)
         sleep(work_minutes)
         pomo_count+=1
-        if pomo_count % 4 != 0:
+        if pomo_count % pomo_count_per_cycle != 0:
             notify()
             Popen(rest_message + f"\nPomodoro Count = {pomo_count}\"", shell=True)
             sleep(rest_minutes)
-            i+=1
         else:
             notify()
             Popen(long_rest_message + f"\nPomodoro Count = {pomo_count}\"", shell=True)
             sleep(long_rest_minutes*60)
-            i=0
